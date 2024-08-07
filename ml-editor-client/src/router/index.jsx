ce7782673c94c6971a992ace0ffaa2dc8f2fe2e8
Eagune <jingdiqkl@163.com>
2024-08-07 16:33:51 +0800

    feat: jsx

--- ml-editor-client/src/router/index.jsx
+++ ml-editor-client/src/router/index.jsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Router } from "@remix-run/router";
 import IDEView from '../views/IDE';
 
-const router: Router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <IDEView />,
