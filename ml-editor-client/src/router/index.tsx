4dd53b0e7a922216508a6dd7bd7cb93f43aaefa8
Eagune <jingdiqkl@163.com>
2024-08-07 15:57:38 +0800

    feat: 添加ide页面

--- ml-editor-client/src/router/index.tsx
+++ ml-editor-client/src/router/index.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Router } from "@remix-run/router";
+import IDEView from '../views/IDE';
 
 const router: Router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <IDEView />,
   },
 ]);
 
