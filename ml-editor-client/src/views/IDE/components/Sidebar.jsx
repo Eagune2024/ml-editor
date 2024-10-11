6034d5106d4aea21e9afaac9b8ac032003f14eae
Eagune <jingdiqkl@163.com>
2024-10-11 17:43:01 +0800

    feat: 新建文件夹Dialog

--- ml-editor-client/src/views/IDE/components/Sidebar.jsx
+++ ml-editor-client/src/views/IDE/components/Sidebar.jsx
@@ -2,6 +2,18 @@ import { useContext } from 'react';
 import classNames from 'classnames';
 import FileNode from './FileNode';
 import { FilesContext } from '..';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 
 export default function SideBar() {
   const { filesValue } = useContext(FilesContext);
@@ -28,7 +40,29 @@ export default function SideBar() {
           </button>
           <ul className="sidebar__project-options">
             <li>
-              <button>新建文件夹</button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <button>新建文件夹</button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>新建文件夹</DialogTitle>
+                  </DialogHeader>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      文件夹名称
+                    </Label>
+                    <Input
+                      id="username"
+                      defaultValue="@peduarte"
+                      className="col-span-3"
+                    />
+                  </div>
+                  <DialogFooter>
+                    <Button type="submit">保存</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
             </li>
             <li>
               <button>新建文件</button>
