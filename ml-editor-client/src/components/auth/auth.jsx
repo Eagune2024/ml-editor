b70146bf853eac21af31b7e8d5ddb2a2ef4c10d6
Eagune <jingdiqkl@163.com>
2024-10-12 17:11:19 +0800

    feat: login

--- ml-editor-client/src/components/auth/auth.jsx
+++ ml-editor-client/src/components/auth/auth.jsx
@@ -1,17 +1,5 @@
-import supabase from "../../supabaseClient";
-
 export default function Auth() {
   return (
-    <form>
-      <div>
-        <Label>邮箱</Label>
-        <Input />
-      </div>
-      <div>
-        <Label>密码</Label>
-        <Input type="password" />
-      </div>
-      <Button>提交</Button>
-    </form>
+    <></>
   )
 }
---
+++ ml-editor-client/src/components/auth/register.jsx
@@ -0,0 +1,41 @@
+import supabase from "../../supabaseClient";
+import React, { useState } from 'react'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
+export default function Auth() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const { error: signInError } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <Label>邮箱</Label>
+        <Input
+          onChange={(e) =>
+            setEmail(e.target.value)
+          }
+        />
+      </div>
+      <div>
+        <Label>密码</Label>
+        <Input
+          type="password"
+          onChange={(e) =>
+            setPassword(e.target.value)
+          }
+        />
+      </div>
+      <Button type="submit">提交</Button>
+    </form>
+  )
+}
