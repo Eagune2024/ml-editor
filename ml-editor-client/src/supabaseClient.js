c3e4b9bc2083be92a5fa67cc2b1bf4d7ad515651
Eagune <jingdiqkl@163.com>
2024-10-30 10:03:37 +0800

    feat: 修改api地址

--- ml-editor-client/src/supabaseClient.js
+++ ml-editor-client/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qeeocqxwcfgurxwhhdvp.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZW9jcXh3Y2ZndXJ4d2hoZHZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2OTUyMDgsImV4cCI6MjA0NDI3MTIwOH0.ygk1DuwcBvokhdKDTcc-D6g6KrMkLOvhtbgZiKEZqM4'
+const supabaseUrl = 'http://jingdiqkl.imwork.net'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
