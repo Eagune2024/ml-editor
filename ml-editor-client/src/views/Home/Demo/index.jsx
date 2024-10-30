3c08f9976b1e064205e2b57e0ebfca528251821d
Eagune <jingdiqkl@163.com>
2024-10-30 14:57:32 +0800

    feat: 代码加载

--- ml-editor-client/src/views/Home/Demo/index.jsx
+++ ml-editor-client/src/views/Home/Demo/index.jsx
@@ -1,3 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
 export default function DemoView () {
-  return (<>demo</>)
+  return (
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="animate-pulse h-[125px] w-[250px] rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="animate-pulse h-4 w-[250px]" />
+        <Skeleton className="animate-pulse h-4 w-[200px]" />
+      </div>
+    </div>
+  )
 }
