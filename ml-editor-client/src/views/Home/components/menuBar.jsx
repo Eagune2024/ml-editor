4e882d4f3238e95d27447fd6224cf809a11a9315
Eagune <jingdiqkl@163.com>
2024-10-30 14:37:09 +0800

    feat: menuBar 样式

--- ml-editor-client/src/views/Home/components/menuBar.jsx
+++ ml-editor-client/src/views/Home/components/menuBar.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
+import { useMatch } from 'react-router-dom';
+import classnames from 'classnames'
 
 export default function MenuBar () {
+  const matchPPT = useMatch('/ppt');
+  const matchNote = useMatch('/note');
+  const matchDemo = useMatch('/demo');
+
+  const pptClassNames = classnames('p-8 h-full hover:bg-violet-100', { 'bg-violet-100': matchPPT })
+  const noteClassNames = classnames('p-8 h-full hover:bg-violet-100', { 'bg-violet-100': matchNote })
+  const demoClassNames = classnames('p-8 h-full hover:bg-violet-100', { 'bg-violet-100': matchDemo })
+
   return (
-    <div>
-      <div className="logo"></div>
-      <div><Link to="ppt">演示</Link></div>
-      <div><Link to="note">笔记</Link></div>
-      <div><Link to="demo">代码</Link></div>
+    <div className="flex items-center bg-slate-50">
+      <Link to="" className="focus:shadow-none">
+        <div className="logo w-40 text-center">Logo</div>
+      </Link>
+      <Link to="ppt" className="focus:shadow-none">
+        <div className={pptClassNames}>演示</div>
+      </Link>
+      <Link to="note" className="focus:shadow-none">
+        <div className={noteClassNames}>笔记</div>
+      </Link>
+      <Link to="demo" className="focus:shadow-none">
+        <div className={demoClassNames}>代码</div>
+      </Link>
     </div>
   )
 }
