fca2d63416461ff2101da683de4cf51cacee37f1
Eagune <jingdiqkl@163.com>
2024-11-08 17:05:36 +0800

    feat: Notebook加载数据

--- ml-editor-client/src/views/Home/Note/index.jsx
+++ ml-editor-client/src/views/Home/Note/index.jsx
@@ -1,9 +1,30 @@
+import { useMemo, useState, Suspense } from "react";
 import { Button } from "@/components/ui/button"
 import { FileTextIcon, FileIcon } from "@radix-ui/react-icons"
-import { useState } from "react"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import WrapPromise from "@/utils/WrapPromise"
+import supabase from "@/supabaseClient";
+
+const BookList = function ({promiseData}) {
+  const { data: bookList, error } = promiseData.read()
+  return (
+    <nav className="grid gap-1 px-2">
+      {
+        bookList.map((book, index) => (
+          <Button variant={ index === currentBook? '': 'ghost' } className="text-lg h-12" key={index}>
+            <FileTextIcon className="mr-2 h-6 w-6" />
+            { book.name }
+            <span className="ml-auto dark:text-white">{ book.note }</span>
+          </Button>
+        ))
+      }
+    </nav>
+  )
+}
 
 export default function NoteView () {
+  const [queryBook, setQueryBook] = useState(Date.now())
+
   const [currentBook, setCurrentBook] = useState(0)
   const [currentNote, setCurrentNote] = useState(0)
 
@@ -53,20 +74,14 @@ export default function NoteView () {
     }
   }
 
+  const promiseData = useMemo(() => WrapPromise(supabase.from('Notebook').select('name, id, created_at')), [queryBook])
+
   return (
     <div className="flex flex-1 border m-4 rounded-lg overflow-hidden">
       <ScrollArea className="group flex flex-col gap-4 py-2 w-72 h-full border-r">
-        <nav className="grid gap-1 px-2">
-          {
-            bookList.map((book, index) => (
-              <Button variant={ index === currentBook? '': 'ghost' } className="text-lg h-12" key={index} onClick={bookClick(index)}>
-                <FileTextIcon className="mr-2 h-6 w-6" />
-                { book.name }
-                <span className="ml-auto dark:text-white">{ book.note }</span>
-              </Button>
-            ))
-          }
-        </nav>
+        <Suspense fallback={<>Loading...</>}>
+          {<BookList promiseData={promiseData} />}
+        </Suspense>
       </ScrollArea>
       <ScrollArea className="h-full">
         <div className="group flex flex-col gap-4 py-2 w-80 border-r">
